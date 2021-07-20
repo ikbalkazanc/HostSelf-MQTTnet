2a5323f1ce99edcc77f018b4781421f7f203a3c5
ikbalkazanc <60016655+ikbalkazanc@users.noreply.github.com>
2021-07-20 14:50:30 +0300

    dockerize

--- BrokerMQTT/Service/Program.cs
+++ BrokerMQTT/Service/Program.cs
@@ -1,7 +1,13 @@
-using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-namespace Broker
+namespace Service
 {
     public class Program
     {
@@ -12,9 +18,9 @@ namespace Broker
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureServices((hostContext, services) =>
+                .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    services.AddHostedService<Worker>();
+                    webBuilder.UseStartup<Startup>();
                 });
     }
 }
